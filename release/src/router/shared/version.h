01d3612ea46cb9be9a417b61de879e47eafdb814
ZhaoWei Xu <paldier@hotmail.com>
2020-07-20 07:52:32 +0800

    softcenter:fix api

--- release/src/router/shared/version.h
+++ release/src/router/shared/version.h
@@ -4,8 +4,8 @@
 #define RT_VERSION "3.0.0.4"
 #define RT_SERIALNO "384"
 #define RT_RCNO "3"
-#define RT_EXTENDNO "RTAC86U_X5.0.9-g2443f3f"
+#define RT_EXTENDNO "RTAC86U_R5.1.1-g9fab649"
 #define RT_SWPJVERNO ""
 #define RT_BUILD_NAME "RT-AC86U"
-#define RT_BUILD_INFO "Sun Mar 15 06:43:03 UTC 2020 paldier@2443f3f"
+#define RT_BUILD_INFO "Sat Jul 18 09:12:11 UTC 2020 paldier@MerlinRdev"
 #endif
