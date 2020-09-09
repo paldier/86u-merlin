689b6e71d9e46992acd2bd21371e03a1c7e8aa64
ZhaoWei Xu <paldier@hotmail.com>
2020-09-09 13:23:35 +0800

    Bumped revision to 5.1.2 beta

--- release/src/router/shared/version.h
+++ release/src/router/shared/version.h
@@ -4,8 +4,8 @@
 #define RT_VERSION "3.0.0.4"
 #define RT_SERIALNO "384"
 #define RT_RCNO "3"
-#define RT_EXTENDNO "RTAC86U_R5.1.1-g01d3612"
+#define RT_EXTENDNO "RTAC86U_R5.1.2b-gd321e43"
 #define RT_SWPJVERNO ""
 #define RT_BUILD_NAME "RT-AC86U"
-#define RT_BUILD_INFO "Mon Jul 20 00:10:22 UTC 2020 paldier@MerlinRdev"
+#define RT_BUILD_INFO "Wed Sep  9 05:08:52 UTC 2020 paldier@MerlinRdev"
 #endif
